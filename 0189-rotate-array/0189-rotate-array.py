ac1e86da9489943962c7e2fe018e4b015f2ff09b
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2023-11-14 00:41:49 +0300

    Time: 181 ms (68.28%), Space: 27.7 MB (73.69%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,6 +4,23 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         k = k % len(nums)
-        nums.reverse()
-        nums[:k] = nums[:k][::-1]
-        nums[k:] = nums[k:][::-1]
+        # nums.reverse()
+        # nums[:k] = nums[:k][::-1]
+        # nums[k:] = nums[k:][::-1]
+#         First reverse nums
+        l, r = 0, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+            
+#             second reverse nums starting from 0 to k-1
+        l, r = 0, k - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
+            
+        #             second reverse nums starting from k to len(nums)
+        l, r = k, len(nums) - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l, r = l + 1, r - 1
