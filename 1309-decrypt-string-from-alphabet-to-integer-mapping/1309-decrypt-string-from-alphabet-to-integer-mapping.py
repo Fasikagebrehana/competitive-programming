09c8249dddce137ea697f9ec5d2ee911d67a1863
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2023-12-05 19:47:06 +0300

    Time: 44 ms (17.53%), Space: 16.2 MB (84.92%) - LeetHub

---
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,17 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        alphabet = {}
+        for u, val in enumerate(range(ord('a'), ord('i') + 1), start = 1 ):
+            alphabet[str(u)] = chr(val)
+        for a, val in enumerate(range(ord('j'), ord('z') + 1), start = 10):
+            alphabet[str(a) + '#'] = chr(val)
+        ans = ""
+        i = 0
+        while i < len(s):
+            if i+2 < len(s) and s[i+2] == '#':
+                ans +=  alphabet[s[i:i+3]]
+                i += 3
+            else:
+                ans += alphabet[s[i]]
+                i += 1
+        return ans
