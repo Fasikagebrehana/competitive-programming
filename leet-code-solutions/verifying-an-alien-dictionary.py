7494177c9ad5b2d4fa348f41ce8ca3d5719d8899
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2023-12-06 22:01:31 +0300

    Added solution for Verifying an Alien Dictionary

---
+++ leet-code-solutions/verifying-an-alien-dictionary.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        ordInd = {c : i for i, c in enumerate(order)}
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i+1]
+            for j in range(len(w1)):
+                if j == len(w2):
+                    return False
+                if w1[j] != w2[j]:
+                    if ordInd[w2[j]] < ordInd[w1[j]]:
+                        return False
+                    break
+        return True
