2cd1ddb40c5a387e7785d92ebc3ac18f3365a6c8
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2023-12-26 18:46:10 +0300

    Time: 30 ms (97.58%), Space: 17.2 MB (5.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,8 +4,8 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i]<nums[j]:
+            for j in range(i, len(nums)):
+                if nums[i]>nums[j]:
                     temp=nums[i]
                     nums[i]=nums[j]
                     nums[j]=temp
