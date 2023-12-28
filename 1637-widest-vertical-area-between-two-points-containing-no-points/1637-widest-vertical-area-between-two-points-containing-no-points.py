82602e16f7746fe995ca5112d38ff781dc8079c1
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2023-12-28 14:38:45 +0300

    Time: 1082 ms (5.10%), Space: 60.4 MB (11.49%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
@@ -5,7 +5,7 @@ class Solution:
         for x,y in points:
             width.append(x)
         width.sort()
-        print(width)
+        
         for i in range(1, len(width)-1):
             dic[i-1] = width[i] - width[i-1]
         return max(dic.values())
