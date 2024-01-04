8398826255794db94e5ae9e8fa0024d3948b04b3
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-01-04 20:06:34 +0300

    Time: 814 ms (85.16%), Space: 20.8 MB (42.39%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,7 +3,9 @@ class Solution:
         
         ans = set()
         nums.sort()
-        for i in range(len(nums)):
+        for i in range(len(nums)-2):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
             target = -nums[i]
             j, k =  i + 1, len(nums)-1
             while j < k:
@@ -11,6 +13,11 @@ class Solution:
                     ans.add((nums[i], nums[j], nums[k]))
                     j += 1
                     k -= 1
+        
+                    while j < len(nums) and nums[j] == nums[j-1]:
+                        j += 1
+                    while k >= 0 and nums[k] == nums[k+1]:
+                        k -= 1
                 elif (nums[j] + nums[k]) > target:
                     k -= 1
                 else:
