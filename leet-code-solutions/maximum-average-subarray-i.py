5868bc959e29c01f06e365bcdeba384940956090
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-01-06 23:52:19 +0300

    Added solution for Maximum Average Subarray I

---
+++ leet-code-solutions/maximum-average-subarray-i.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        curr_avg = sum(nums[:k])
+        max_avg = curr_avg
+        for i in range(k, len(nums)):
+            curr_avg += nums[i]  
+            curr_avg -= nums[i-k]
+            max_avg = max(max_avg, curr_avg)
+        return max_avg / k
