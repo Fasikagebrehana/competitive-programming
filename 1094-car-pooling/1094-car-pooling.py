8d6996de67b870d3ae95133a7b7fca9edf230a17
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-01-21 21:37:48 +0300

    Time: 69 ms (76.92%), Space: 17.1 MB (55.29%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,17 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        totalpassengers = 0
+        max_end = max(trip[2] for trip in trips)
+        temp = [0] * (max_end + 1)
+        for passengers, start, end in trips:
+            temp[start] += passengers
+            temp[end] -= passengers
+        # print(temp)
+        for i in range(1, len(temp)):
+            temp[i] += temp[i-1]
+        # print(temp)
+        if max(temp) > capacity:
+            return False
+        else:
+            return True
+        
