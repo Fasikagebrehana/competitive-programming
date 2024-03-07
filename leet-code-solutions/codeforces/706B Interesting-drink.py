5afb2d5ce8eb3ff3877f68cf7382623d9af2b62e
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-03-07 18:49:57 +0300

    Add solution for Interesting drink

--- leet-code-solutions/codeforces/706B Interesting-drink.py
+++ leet-code-solutions/codeforces/706B Interesting-drink.py
@@ -3,9 +3,6 @@ n = int(input())
 price = list(map(int, input().split()))
 q = int(input())
 price.sort()
-
 for _ in range(q):
     p = int(input())
     print(bisect_right(price, p))
-    
-            
