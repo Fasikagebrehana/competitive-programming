3c235316ca2ad6806c3d5240a6dbde287e2ceda3
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-03-07 19:07:39 +0300

    Add solution for Interestingdrink

---
+++ leet-code-solutions/Codeforces/Interestingdrink.py
@@ -0,0 +1,9 @@
+from bisect import bisect_right
+n = int(input())
+price = list(map(int, input().split()))
+q = int(input())
+price.sort()
+
+for _ in range(q):
+    p = int(input())
+    print(bisect_right(price, p))
