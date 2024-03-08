149429ad529a985edb0ad4edb727ff513514afa5
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-03-08 12:01:43 +0300

    Add solution for Find K Closest Elements

--- leet-code-solutions/leetcode/find-k-closest-elements.py
+++ leet-code-solutions/leetcode/find-k-closest-elements.py
@@ -2,11 +2,22 @@ class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         idx = bisect_left(arr, x)
         ans = []
-        for i in range(len(arr)):
-            ans.append((abs(arr[i]-x), arr[i]))
+        left, right = idx - 1, idx
+        while left >= 0 or right < len(arr):
+            if len(ans) == k:
+                break
+            if left < 0:
+                ans.append(arr[right])
+                right += 1
+            elif right >= len(arr):
+                ans.append(arr[left])
+                left -= 1
+            else:
+                if x - arr[left] > abs(x - arr[right]):
+                    ans.append(arr[right])
+                    right += 1
+                else:
+                    ans.append(arr[left])
+                    left -= 1
         ans.sort()
-        res = []
-        for i in range(k):
-            res.append(ans[i][1])
-        res.sort()
-        return res
+        return ans
