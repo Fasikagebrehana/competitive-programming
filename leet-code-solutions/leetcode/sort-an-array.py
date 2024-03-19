d2e7cf6a6644e7665a47b79a3a305ebc2340c96a
Fasikagebrehana <105711954+Fasikagebrehana@users.noreply.github.com>
2024-03-19 19:20:25 +0300

    Add solution for Sort an Array

---
+++ leet-code-solutions/leetcode/sort-an-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def merge(lhalf, rhalf):
+            ans = []
+            l, r = 0, 0
+            while l < len(lhalf) and r < len(rhalf):
+                if lhalf[l] > rhalf[r]:
+                    ans.append(rhalf[r])
+                    r += 1
+                else:
+                    ans.append(lhalf[l])
+                    l += 1
+            ans.extend(lhalf[l:])
+            ans.extend(rhalf[r:])
+            return ans
+
+        def helper(arr, l, r):
+            if l == r:
+                return [arr[l]]
+            middle = (l + r) // 2
+            left = helper(arr, l, middle)
+            right = helper(arr, middle + 1, r)
+            return merge(left, right)
+        
+
+        return helper(nums, 0, len(nums) - 1)
