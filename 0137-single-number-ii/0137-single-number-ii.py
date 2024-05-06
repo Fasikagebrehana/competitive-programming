bded622b115b5a01138a30407344fad59c901bc1
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-05-06 17:40:48 +0300

    Time: 59 ms (53.68%), Space: 18.8 MB (20.62%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,6 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        for key, val in counter.items():
+            if val == 1:
+                return key
