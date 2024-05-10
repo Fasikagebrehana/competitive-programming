bb7692d56a7f45983482f39c846109998d37d09b
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-05-10 13:08:12 +0300

    Time: 32 ms (74.47%), Space: 16.5 MB (32.45%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,8 +1,12 @@
 class Solution:
-    @cache
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
-        return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+        store = {}
+        def fib(n):
+            if n == 0:
+                return 0
+            if n == 1 or n == 2:
+                return 1
+            if n not in store:
+                store[n] = fib(n-1) + fib(n-2) + fib(n-3)
+            return store[n]
+        return fib(n)
