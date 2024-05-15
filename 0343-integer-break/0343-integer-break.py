6dddbb365e720a21402b0311fb2879c49be93548
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-05-15 19:36:56 +0300

    Time: 40 ms (35.85%), Space: 16.6 MB (43.96%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        store = {}
+        if n < 4:
+            return n - 1
+        def dp(num):
+            if num < 4:
+                return num
+            
+            if num not in store:
+                store[num] = 0
+                for i in range(1, n + 1):
+                    store[num] = max(store[num], i * dp(num - i))
+
+            return store[num]
+        return dp(n)
