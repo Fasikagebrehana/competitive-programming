05fcd7392467d2d2eabf03175434754512fe0f7c
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-05-16 18:03:22 +0300

    Time: 34 ms (73.86%), Space: 16.5 MB (26.79%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -2,10 +2,12 @@ class Solution:
     def fib(self, n: int) -> int:
         if n < 2:
             return n
-        dp = [0] * (n + 1)
-        dp[0] = 0
-        dp[1] = 1
+        # dp = [0] * (n + 1)
+        first = 0
+        second = 1
 
         for i in range(2, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[-1]
+            temp = first + second
+            first = second
+            second = temp
+        return second
