2c49244fde137639b73d09533b5bdc40d44af0c9
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-05-20 19:42:49 +0300

    Time: 48 ms (40.35%), Space: 17.9 MB (33.5%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        store = {}
+        n = len(prices)
+        def profit(i, state):
+            if i  >= n:
+                return 0
+            if (i, state) not in store:
+                if not state:
+                    store[((i, state))] = max(profit(i + 1, not state) - prices[i], profit(i+1, state))
+                else:
+                    store[((i, state))] = max(profit(i + 2, not state) + prices[i], profit(i+1, state))
+            return store[(i, state)]
+        return profit(0, False)
