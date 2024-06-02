9b4c9146caafa4569743643391c409eaf2e751ca
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-06-02 22:15:02 +0300

    Time: 1052 ms (6.99%), Space: 33.1 MB (8.35%) - LeetHub

---
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        ans = [-1] * len(nums)
+        median = nums[len(nums) // 2]
+        less = []
+        for i in range(len(nums) // 2):
+            less.append(nums[i])
+        print(less)
+        greater = []
+        for i in range(len(nums) // 2, len(nums)):
+            greater.append(nums[i])
+        greater = greater[::-1]
+        l = 0
+        i = 0
+        while i < len(ans) and l < len(less):
+            ans[i] = less[l]
+            l += 1
+            i += 2
+        
+        r = 0
+        i = 0
+        while i < len(ans) and r < len(greater):
+            if ans[i] == -1:
+                ans[i] = greater[r]
+                r += 1
+            i += 1
+        return ans
+        
