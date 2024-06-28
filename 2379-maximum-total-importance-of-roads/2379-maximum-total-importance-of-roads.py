7b295a5804de36435da151ac022cd250fcb2e5b5
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-06-28 18:47:21 +0300

    Time: 1237 ms (59.84%), Space: 41 MB (95.49%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.py
@@ -1,10 +1,7 @@
 class Solution:
     def maximumImportance(self, n: int, roads: List[List[int]]) -> int:
-        adjList = defaultdict(list)
         indegree = [0] * n
         for x, y in roads:
-            adjList[x].append(y)
-            adjList[y].append(x)
             indegree[x] += 1
             indegree[y] += 1
         value = []
@@ -16,9 +13,7 @@ class Solution:
         for x, y in value:
             values[y] = temp
             temp -= 1
-        visited = set()
         max_sum = 0
         for x, y in roads:
             max_sum += values[x] + values[y]
-            # visited.add((x, y))
         return max_sum
