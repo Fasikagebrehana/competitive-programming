08920f53988e1b6046e15db3eb3b5182ca80151f
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-07-01 11:41:10 +0300

    Time: 1118 ms (25.46%), Space: 54.8 MB (34.72%) - LeetHub

--- 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,19 +1,17 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
-        directions = [[(0, -1), (0, 1)], [(-1, 0), (1, 0)], [(0, -1), (1, 0)], [(0, 1), (1, 0)], [(0, -1), (-1, 0)], [(0, 1), (-1, 0)]]
+        directions = {1: [(0, -1), (0, 1)], 2: [(-1, 0), (1, 0)], 3: [(0, -1), (1, 0)], 4: [(0, 1), (1, 0)], 5: [(0, -1), (-1, 0)], 6: [(0, 1), (-1, 0)]}
         def inbound(r, c):
             return (0 <= r < len(grid) and 0 <= c < len(grid[0]))
         visited = set()
         def dfs(row, col):
             visited.add((row, col))
-            if (row, col) == (len(grid) -1, len(grid[0]) - 1):
+            if (row, col) == (len(grid) - 1, len(grid[0]) - 1):
                 return True
-            for a, b in directions[grid[row][col] - 1]:
-                newRow, newCol = row + a, col + b
-                if inbound(newRow, newCol) and (newRow, newCol) not in visited:
-                    for x, y in directions[grid[newRow][newCol] - 1]:
-                        if (newRow + x, newCol + y) == (row, col):
-                            if dfs(newRow, newCol):
-                                return True
+            
+            for x, y in directions[grid[row][col]]:
+                if inbound(x + row, y + col) and (-x, -y) in directions[grid[row + x][col + y]] and (x + row, y + col) not in visited:
+                    if dfs(x + row, y + col):
+                        return True
             return False
         return dfs(0, 0)
