25cc91a3f75fd1b3474f8dc016342ef13d61de5c
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2024-09-02 11:16:49 +0300

    Time: 570 ms (74.41%), Space: 29.9 MB (84.17%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.py
@@ -16,18 +16,30 @@ class Solution:
         #     middle = (right - left) // 2
         #     if check(middle):
 
-        prefix = [0] * len(chalk)
-        prefix[0] = chalk[0]
-        for i in range(1, len(chalk)):
-            prefix[i] += chalk[i] + prefix[i-1]
-        if len(chalk) == 1:
-            return 0
-        while k > 0:
-            idx = bisect_left(prefix, k)
-            if idx >= len(chalk):
-                k -= prefix[-1]
-            else:
-                if k == prefix[idx]:
-                    return (idx + 1) % len(chalk)
-                else:
-                    return idx
+        # prefix = [0] * len(chalk)
+        # prefix[0] = chalk[0]
+        # for i in range(1, len(chalk)):
+        #     prefix[i] += chalk[i] + prefix[i-1]
+        # if len(chalk) == 1:
+        #     return 0
+        # while k > 0:
+        #     idx = bisect_left(prefix, k)
+        #     if idx >= len(chalk):
+        #         k -= prefix[-1]
+        #     else:
+        #         if k == prefix[idx]:
+        #             return (idx + 1) % len(chalk)
+        #         else:
+        #             return idx
+        # while prefix = [0] * len(chalk)
+        # prefix[0] = chalk[0]
+        #             return (idx + 1) % len(chalk)
+        #         else:
+        #             return idx
+
+        k %= sum(chalk)
+        for i in range(len(chalk)):
+            if k < chalk[i]:
+                return i
+            k -= chalk[i]
+        return
