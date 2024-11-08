fbc030aff624d3744c8ebbda91e274b30b7c42c7
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2024-11-08 09:54:16 +0300

    Time: 246 ms (84.21%), Space: 19.8 MB (41.79%) - LeetHub

--- 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
+++ 0318-maximum-product-of-word-lengths/0318-maximum-product-of-word-lengths.py
@@ -1,11 +1,20 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
         ans = 0
+        def helper(word):
+            bit = 0
+            for ch in word:
+                bit |= 1 << (ord(ch) - ord('a'))
+            return bit
+
+        arr = []
+        for word in words:
+            temp = helper(word)
+            arr.append(temp)
+        
         for i in range(len(words)):
             for j in range(i+1, len(words)):
-                for ch in words[i]:
-                    if ch in words[j]:
-                        break
-                else:
+                if not arr[i] & arr[j]:
                     ans = max(ans, len(words[i]) * len(words[j]))
+        
         return ans
