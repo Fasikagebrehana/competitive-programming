3bdf442334e037e61c56ffa8e274f88b46f30b69
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2024-11-26 21:35:13 +0300

    Time: 123 ms (55.54%), Space: 57.6 MB (32.21%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,23 +1,32 @@
 class RandomizedSet:
 
     def __init__(self):
-        
-        self.nums = set()
+        self.dic = {}
+        self.nums = []
+
     def insert(self, val: int) -> bool:
-        if val in self.nums:
+        if val in self.dic:
             return False
         else:
-            self.nums.add(val)
+            self.nums.append(val)
+            self.dic[val] = len(self.nums) - 1
+            return True
 
     def remove(self, val: int) -> bool:
-        if val in self.nums:
-            self.nums.remove(val)
+        if val in self.dic:
+            # temp = self.dic[self.nums[-1]]
+            idx = self.dic[val]
+            num = self.nums[-1]
+            self.nums[idx], self.nums[-1] = self.nums[-1], self.nums[idx]
+            self.dic[num] = idx
+            self.dic[val] = len(self.nums) - 1
+            self.nums.pop()
+            del self.dic[val]
             return True
         return False
-
+        
     def getRandom(self) -> int:
-        self.data = list(self.nums)
-        return random.choice(self.data)
+        return random.choice(self.nums)
         
 
 
