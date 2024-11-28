8869b441186ca827ed98d48c08b91dcdf48ff0df
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2024-11-29 00:22:26 +0300

    Time: 107 ms (89.1%), Space: 30.6 MB (35.04%) - LeetHub

--- 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.py
@@ -1,14 +1,21 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
         nums.sort()
-        answer = 0
+        return self.lower_bound(nums, upper + 1) - self.lower_bound(nums, lower)
 
-        for i in range(len(nums)-1):
-        #     if nums[i] > upper:
-        #         return answer
-            j = bisect_left(nums, lower - nums[i], i+1)
-            idx = bisect_right(nums, upper - nums[i], i+1)
-            # print(idx, j)
-            if idx >= j:
-                answer += idx - j
-        return answer
+    # Calculate the number of pairs with sum less than `value`.
+    def lower_bound(self, nums: List[int], value: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        result = 0
+        while left < right:
+            sum = nums[left] + nums[right]
+            # If sum is less than value, add the size of window to result and move to the
+            # next index.
+            if sum < value:
+                result += right - left
+                left += 1
+            else:
+                # Otherwise, shift the right pointer backwards, until we get a valid window.
+                right -= 1
+        return result
