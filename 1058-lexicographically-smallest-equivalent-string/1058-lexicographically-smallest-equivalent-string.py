1187148ba0b66dab2b8d75c353a22121fd466f3e
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2024-11-30 23:04:12 +0300

    Time: 7 ms (49.02%), Space: 17.2 MB (5.67%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
@@ -1,28 +1,37 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        a = ord('a')
-        parent = {chr(a): chr(a) for a in range(ord('a'), ord('z') + 1)}
-        
-        
+        parent = {i: i for i in range(26)}
+        size = [1] * (26)
         def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-        
+            while x != parent[x]:
+                parent[x] = parent[parent[x]]
+                x = parent[x]
+            return x
+
         def union(x, y):
-            parentX = find(x)
-            parentY = find(y)
+            parentX, parentY = find(x), find(y)
+
             if parentX != parentY:
-                if ord(parentX) <= ord(parentY):
+                if parentX <= parentY:
                     parent[parentY] = parentX
+                    size[parentX] += size[parentY]
                 else:
                     parent[parentX] = parentY
+                    size[parentY] += size[parentX]
 
         for i in range(len(s1)):
-            union(s1[i], s2[i])
-        print(parent)
-
-        ans = ""
+            a = ord(s1[i]) - ord('a')
+            # print(ord(s1[i]), ord('a'))
+            # print(a)
+            b = ord(s2[i]) - ord('a')
+            union(a, b)
+        # print(parent)
+        answer = []
         for s in baseStr:
-            ans += find(s)
-        return ans
+            p = ord(s) - ord('a')
+            # print(p)
+            ch = find(p)
+            # print(ch)
+            ch += ord('a')
+            answer.append(chr(ch))
+        return ''.join(answer)
