82479aeebf92f2c2ac2c3e7372cd118fbdef9d90
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2024-12-03 21:47:16 +0300

    Time: 107 ms (50.73%), Space: 48.9 MB (40.48%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.py
@@ -1,11 +1,11 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        answer = ""
+        store = []
         idx = 0
         for i in range(len(s)):
             if idx < len(spaces) and i == spaces[idx]:
-                # print("dfggtd")
-                answer += " "
+                store.append(' ')
                 idx += 1
-            answer += s[i]
-        return answer
+            store.append(s[i])
+        
+        return ''.join(store)
