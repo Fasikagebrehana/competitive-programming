bc95c643f6e3551aaeeeadd95698b11fccd1c6de
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-02-06 12:20:34 +0300

    Time: 387 ms (48.34%), Space: 46.5 MB (41.71%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -1,15 +1,15 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        store = defaultdict(list)
+        store = defaultdict(int)
         count = 0
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
-                store[nums[i] * nums[j]].append((nums[i], nums[j]))
+                store[nums[i] * nums[j]] += 1
                 # store[nums[i] * nums[j]].append((nums[j], nums[i]))
         
         for key, val in store.items():
-            if len(val) > 1:
-                x = (len(val) * (len(val) - 1)) // 2
+            if val > 1:
+                x = (val * (val - 1)) // 2
                 count += (x * 8)
                 
         return count
