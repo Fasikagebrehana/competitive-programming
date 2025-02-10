7eebf8c734231076098b568385af2ad7c7fe038b
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-02-10 23:32:54 +0300

    Time: 0 ms (100%), Space: 17.8 MB (44.2%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            if s[i].isalpha():
+                stack.append(s[i])
+            else:
+                if stack and stack[-1].isalpha():
+                    stack.pop()
+        return ''.join(stack)
