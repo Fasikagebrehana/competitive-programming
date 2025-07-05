cdf4ddb445692ae09f9763783c7baf0c7c046901
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-07-05 23:27:30 +0300

    Time: 1878 ms (5.09%), Space: 18.1 MB (6.04%) - LeetHub

--- 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
+++ 1058-lexicographically-smallest-equivalent-string/1058-lexicographically-smallest-equivalent-string.py
@@ -1,37 +1,39 @@
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        parent = {i: i for i in range(26)}
-        size = [1] * (26)
-        def find(x):
-            while x != parent[x]:
-                parent[x] = parent[parent[x]]
-                x = parent[x]
-            return x
+        graph = defaultdict(list)
+        for x,y in zip(s1, s2):
+            graph[x].append(y)
+            graph[y].append(x)
+        # print(graph)
 
-        def union(x, y):
-            parentX, parentY = find(x), find(y)
+        def bfs(l):
+            queue = deque([l])
+            visited = set()
+            min_char = l
+            while queue:
+                node = queue.popleft()
+                visited.add(node)
 
-            if parentX != parentY:
-                if parentX <= parentY:
-                    parent[parentY] = parentX
-                    size[parentX] += size[parentY]
-                else:
-                    parent[parentX] = parentY
-                    size[parentY] += size[parentX]
+                for neigh in graph[node]:
+                    if neigh not in visited:
+                        min_char = min(min_char, neigh)
+                        queue.append(neigh)
 
-        for i in range(len(s1)):
-            a = ord(s1[i]) - ord('a')
-            # print(ord(s1[i]), ord('a'))
-            # print(a)
-            b = ord(s2[i]) - ord('a')
-            union(a, b)
-        # print(parent)
+            return min_char
+
+        store = defaultdict(str)
         answer = []
+        visiteds = set()
         for s in baseStr:
-            p = ord(s) - ord('a')
-            # print(p)
-            ch = find(p)
-            # print(ch)
-            ch += ord('a')
-            answer.append(chr(ch))
+            if s not in visiteds:
+                minChar = bfs(s)
+                answer.append(minChar)
+                store[s] = minChar
+                visiteds.add(s)
+            else:
+                answer.append(store[s])
+            
+            
+        # print(answer)
+
         return ''.join(answer)
