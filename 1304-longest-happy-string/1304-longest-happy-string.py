d87d646169a125634c9c89cde3da58656ae242e3
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-07-28 14:45:01 +0300

    Time: 2 ms (18.52%), Space: 17.9 MB (24.52%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -1,29 +1,32 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        ans = []
-        
         heap = []
-        if a > 0:
+        if a >0:
             heappush(heap, (-a, 'a'))
         if b > 0:
             heappush(heap, (-b, 'b'))
-        if c > 0:
+        if c >0:
             heappush(heap, (-c, 'c'))
 
+        happy = []
         while heap:
             count, ch = heappop(heap)
-            if len(ans) >= 2 and ans[-1] == ch and ans[-2] == ch:
+            if len(happy) >= 2 and happy[-1] == ch and happy[-2] == ch:
                 if heap:
                     count2, ch2 = heappop(heap)
-                    ans.append(ch2)
+                    happy.append(ch2)
                     count2 += 1
                     if count2 < 0:
                         heappush(heap, (count2, ch2))
+                    # even if count2 is 0 or greater since we already appended ch2 we can push  ch and count to the heap
                     heappush(heap, (count, ch))
             else:
-                ans.append(ch)
+                happy.append(ch)
                 count += 1
                 if count < 0:
+                    # if the count is still not zero we append it to heap and again we append it to the heap and on the next pop it will pop the max so it can be ch or other
                     heappush(heap, (count, ch))
-        return ''.join(ans)
+            
+        # print(happy)
 
+        return ''.join(happy)
