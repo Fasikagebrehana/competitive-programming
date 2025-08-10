37665578fb25826c9f587ddb2ff0de08b1debeea
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-08-10 16:02:03 +0300

    Time: 11 ms (6.64%), Space: 17.8 MB (86.99%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -4,15 +4,13 @@ class Solution:
         # starting from the last index, check each slice if its true
 
         dp = [False] * (len(s) + 1)
-        dp[-1] = True
-        for i in range(len(s) - 1, -1, -1):
-            for word in wordDict:
-                if (i + len(word)) <= len(s):
-                    if word == s[i: i + len(word)]:
-                        # example 1: checking code(s[4:8]) == wordDict[1]
-                        dp[i] = dp[i+len(word)]
-                if dp[i] == True:
+        dp[0] = True
+        for i in range(1, len(s)+1):
+            for j in range(i):
+                if s[j:i] in wordDict and dp[j]:
+                    dp[i] = True
                     break
 
         # if s can be segmented from wordDict dp[0] will be true
-        return dp[0] 
+        # print(dp)
+        return dp[len(s)] 
