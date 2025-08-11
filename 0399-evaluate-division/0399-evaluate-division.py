19049c301d66162ce705adaa61d049b348678e28
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-08-11 14:41:38 +0300

    Time: 0 ms (100%), Space: 17.9 MB (73.05%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,43 +1,40 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        graph = defaultdict(list)
+        # create the connection between the equations then create our adjacency relation 
 
+        graph = defaultdict(list)
         for i in range(len(equations)):
-            a, b = equations[i]
-            graph[a].append((b, values[i]))
-            graph[b].append((a, 1 / values[i]))
-        
-        # print(graph)
-        
+            x, y = equations[i]
+            graph[x].append((y, values[i]))
+            graph[y].append((x, 1 / values[i]))
+
+        # now we need to use a data structure that will traverse through the graph then try to find the target for each queries
         visited = set()
-        def dfs(curr, target, ans):
-            nonlocal result
-            if curr == target:
-                result = ans
-                return
-            visited.add(curr)
+
+        def dfs(num, target, result):
+            if num == target:
+                return result
             
-            for neigh, weigh in graph[curr]:
+            for neigh, weigh in graph[num]:
                 if neigh not in visited:
-                    dfs(neigh, target, ans * weigh)
-        evaluation = [] 
+                    visited.add(neigh)
+                    # we need to store it to a temporary variable and return, so that it can be returned to the previous caller 
+                    temp = dfs(neigh, target, result * weigh)
+                    if temp:
+                        return temp
+            
+
+        answer = []
         for x, y in queries:
-            result = None
             ans = 1
             if x not in graph or y not in graph:
-                evaluation.append(-1.0)
-                continue
+                answer.append(-1)
             elif x == y:
-                evaluation.append(1.0)
-                continue
+                answer.append(1.0)
             else:
                 visited = set()
-                if x not in visited:
-                    visited.add(x,)
-                    dfs(x, y, ans)
-                   
-            if result is not None:
-                evaluation.append(result)
-            else:
-                evaluation.append(-1)
-        return evaluation
+                visited.add(x)
+                ans = dfs(x, y, ans)
+                answer.append(ans) if ans else answer.append(-1)
+        # print(answer)
+        return answer
