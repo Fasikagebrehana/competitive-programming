212d5e6fa5f46e35111f4a6ed6cc1c28088c82f7
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-08-11 18:44:35 +0300

    Time: 3 ms (19.33%), Space: 21.2 MB (93.32%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -6,21 +6,28 @@
 #         self.right = right
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        # good strategy is to replace the tobe deleted val with the lowest value in the right branch of the tobedeleted node
+        # but first we have to search the val
+
         if not root:
-            return None
-        if key < root.val:
+            return
+        if root.val > key:
             root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        elif key == root.val:
-            if not root.right:
-                return root.left
-            elif not root.left:
+        elif root.val < key:
+            root.right =  self.deleteNode(root.right, key)
+        else:
+            # here we have 3 options, 1. if it has only left child
+            # 2. it has only right child 3. both
+            if not root.left:
                 return root.right
+            elif not root.right:
+                return root.left
             else:
-                temp = root.right
-                while temp.left:
-                    temp = temp.left
-                root.val = temp.val
-                root.right = self.deleteNode(root.right, temp.val)
+                # if it has 2 branches we have to fing the minimum from the right child
+                # successor = root
+                successor = root.right
+                while successor.left:
+                    successor = successor.left
+                root.val = successor.val
+                root.right = self.deleteNode(root.right, root.val)
         return root
