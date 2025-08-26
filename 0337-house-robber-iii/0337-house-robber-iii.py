c3bf3f6460542d1e4c8ade2645f6a80615ba321f
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-08-26 16:53:41 +0300

    Time: 8 ms (24.09%), Space: 20.1 MB (29.34%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,6 +6,13 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
+        # 2 choices here
+        # if we take the curr node then we have to move the left of left and to the left then right
+        # also right of left and right
+        # else if we didn't take curr we have 2 options take the summ of the left and the right
+
+        # then take the maximum of the two
+
         memo = {}
         def dp(root):
             if not root:
@@ -20,7 +27,7 @@ class Solution:
             if root.right:
                 money_with_curr += dp(root.right.right) + dp(root.right.left)
             money_without_curr = dp(root.left) + dp(root.right)
-            
+
             memo[root] = max(money_with_curr, money_without_curr)
 
             return memo[root]
