8c1a44c6e45cc58c852c546d7f0c5be667d223bc
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-09-02 14:11:33 +0300

    Time: 156 ms (37.6%), Space: 78.9 MB (8.56%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,48 +1,71 @@
-class LinkedList:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.head = None
-        self.prev = None
+# on the listnode we need this to create a new node which have a next, prev, key and value
+class ListNode:
+    def __init__(self, key, value):
         self.next = None
+        self.prev = None
+        self.key = key
+        self.value = value
 
 class LRUCache:
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.left = LinkedList(0, 0)
-        self.right = LinkedList(0, 0)
-        self.left.next = self.right
-        self.right.prev = self.left
-        self.store = {}
-    
-    def remove(self, node):
-        prev = node.prev
-        nxt = node.next
-        prev.next = nxt
-        nxt.prev = prev
-    
+    # for the recent access use linkedlist
+    # 1: ListNode()
+    # for the count part use dictionary 
+    # we need two functions remove for removing a node and to add a node to the listnode
+    # ??when do we create head and tail 
 
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+    def __init__(self, capacity: int):
+        self.store = {}
+        self.capacity = capacity
+        self.head = ListNode(0, 0)
+        self.tail = ListNode(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+
+        
+    def remove(self, key):
+        node = self.store[key]
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    def add(self, key, val):
+        # we create a new listnode for the 
+        #  value we create a new node with the key and value
+        # assign the prev to the tail.prev and the next to tail
+        newnode = ListNode(key, val)
+        newnode.prev = self.tail.prev
+        self.tail.prev.next = newnode
+        newnode.next = self.tail
+        self.tail.prev = newnode
+        self.store[key] = newnode
+        
 
 
     def get(self, key: int) -> int:
-        if key in self.store:
-            self.remove(self.store[key])
-            self.insert(self.store[key])
-            return self.store[key].val
-        return -1
+        if key not in self.store:
+            return -1
+        self.remove(key)
+        value = self.store[key].value
+        del self.store[key]
+        self.add(key, value)
+        return value
+        
+
     def put(self, key: int, value: int) -> None:
         if key in self.store:
-            self.remove(self.store[key])
-        self.store[key] = LinkedList(key, value)
-        self.insert(self.store[key])
+            self.remove( key)
+
+        self.add(key, value)
+
         if len(self.store) > self.capacity:
-            lru = self.left.next
-            self.remove(lru)
-            del self.store[lru.key]
+            # print(self.head.next.key)
+            temp = self.head.next
+            self.head.next = temp.next
+            temp.next.prev = self.head
+            # print(temp)
+            print(self.tail.key)
+            if temp.key in self.store:
+                del self.store[temp.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
