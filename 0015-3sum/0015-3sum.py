451b6ef19ea07b0ef74b22709c37144639800fb2
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-10-15 13:54:28 +0300

    Time: 2649 ms (5%), Space: 20.2 MB (94.37%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,18 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        ans = set()
+        # for each element in nums we assume its target and do two sum on the rest
+        answer = set()
         nums.sort()
         for i in range(len(nums)-2):
-            if i != 0 and nums[i] == nums[i-1]:
-                continue
-            target = -nums[i]
-            j, k =  i + 1, len(nums)-1
-            while j < k:
-                if nums[j] + nums[k] == target:
-                    ans.add((nums[i], nums[j], nums[k]))
-                    j += 1
-                    k -= 1
-        
-                    while j < len(nums) and nums[j] == nums[j-1]:
-                        j += 1
-                    while k >= 0 and nums[k] == nums[k+1]:
-                        k -= 1
-                elif (nums[j] + nums[k]) > target:
-                    k -= 1
-                else:
-                    j += 1
-            
-        return list(ans)
+            store = defaultdict(int)
+            target = -(nums[i])
+            for j in range(i+1, len(nums)):
+                if nums[j] in store:
+                    ans = (nums[i], nums[j], nums[store[nums[j]]])
+                    answer.add(ans)
+                store[target - nums[j]] = j
+        # print(answer)
+
+        return list(answer)
+        # time complexity = sorting nlogn + n^2 ~= n^2
+        # space complexity = 2n ~= n best case
