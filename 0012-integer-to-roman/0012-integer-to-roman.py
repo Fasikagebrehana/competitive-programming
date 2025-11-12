3f6585bdadea2260258b4c83a148206afe759777
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2025-11-12 15:00:37 +0300

    Time: 0 ms (100%), Space: 17.7 MB (69.07%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,53 +1,20 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        # what if we change the numebr to their multipliers
-        # like 3749 = 3000 + 700 + 40 + 9
-        # when to substract => if number starts with 4 or 9
+        # to compute in O(1) time
+        # precompute the essential romans and numbers including numbers related to 4 and 9
+        
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        romans = ["M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"]
 
-        # store values of the roman values with their respective signs
-        store = {1: 'I', 5: 'V', 10:'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
-        roman = [1, 5, 10, 50, 100, 500, 1000]
-
-        nums = []
-        multiplier = 1
-        while num > 0:
-            rem = num % 10
-            num = num // 10
-            nums.append(rem * multiplier)
-            multiplier *= 10
-        nums = nums[::-1]
-        # print(nums)
-        # ?? how to know which to choose
-        # thinking of using binary search but it might be too much
-        # is there any other way to find the corresponding number from store for each number inside nums
+        # iterate through the values and if its greater or equal we append corresponding roman symbol to our answer 
         answer = []
-        for num in nums:
-            i = bisect_left(roman, num)
-            if i == len(roman):
-                n = int(str(num)[0])
-                answer.append('M' * n)
-            elif str(num)[0] == '4' or str(num)[0] == '9':
-                answer.append(store[roman[i]- num] )
-                answer.append(store[roman[i]])
-            else:
-                # print(num)
-                if num in roman:
-                    answer.append(store[num])
-                    continue
-                summ = 0
-                nn = num
-                while summ < nn:
-                    i = bisect_left(roman, num)
-                    # print(i)
-                    if num in roman:
-                        answer.append(store[num])
-                        num -= roman[i]
-                        summ += roman[i]
-                        continue
-                    answer.append(store[roman[i-1]])
-                    num -= roman[i-1]
-                    summ += roman[i-1]
-                    # print(summ)
+
+        for i in range(len(values)):
+            while num >= values[i]:
+                answer.append(romans[i])
+                num -= values[i]
         # print(answer)
         return ''.join(answer)
-                
