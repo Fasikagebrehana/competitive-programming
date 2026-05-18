a8f6856139a352ae35c7937e5fc212ea1e99fe4d
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2026-05-18 17:13:35 +0300

    Time: 7 ms (71.9%), Space: 22.3 MB (97.36%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,15 +1,28 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        merged = [intervals[0]]
+        # merged = [intervals[0]]
         
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= merged[-1][1]:
-                merged[-1][1] = max(merged[-1][1], intervals[i][1])
+        # for i in range(1, len(intervals)):
+        #     if intervals[i][0] <= merged[-1][1]:
+        #         merged[-1][1] = max(merged[-1][1], intervals[i][1])
             
+        #     else:
+        #         merged.append(intervals[i])
+        # # print(merged)
+        # # time complexity - O(n log n) for the sorting O(n) traverse, appending amortized O(1) so its O(n log n)
+        # # space complexity - O(n) because of using merged and No merging happens.
+        # return merged
+
+        # to solve it without using extra space 
+
+        last_merged = 0
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= intervals[last_merged][1]:
+                intervals[last_merged][1] = max(intervals[last_merged][1], intervals[i][1])
             else:
-                merged.append(intervals[i])
-        # print(merged)
-        # time complexity - O(n log n) for the sorting O(n) traverse, appending amortized O(1) so its O(n log n)
-        # space complexity - O(n) because of using merged and No merging happens.
-        return merged
+                last_merged += 1
+                intervals[last_merged] = intervals[i]
+        # print(intervals[:last_merged+ 1])
+
+        return intervals[:last_merged+ 1]
