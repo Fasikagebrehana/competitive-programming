53c339331d565acb4b32c2262225cbd8fb5384a0
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2026-05-27 13:08:49 +0300

    Time: 268 ms (52.94%), Space: 21.2 MB (86.47%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -1,20 +1,20 @@
 class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         # keep track of the index of each appearance of characters
-        lowers = defaultdict(list)
-        uppers = defaultdict(list)
-        # print(ord('z'))
+        lowers = defaultdict(int)
+        uppers = defaultdict(int)
+        print(ord('A'))
 
         for i in range(len(word) - 1, -1, -1):
-            if 97 <= ord(word[i]) < 123:
-                lowers[word[i]].append(i)
-            else:
-                uppers[word[i]].append(i)
+            if word[i] not in lowers and 97 <= ord(word[i]) < 123:
+                lowers[word[i]] = i
+            elif ord(word[i]) < 97:
+                uppers[word[i]] = i
 
         # print(lowers)
         # print(uppers)
         special = 0
         for ch, idx in lowers.items():
-            if ch.upper() in uppers and max(idx) < min(uppers[ch.upper()]):
+            if ch.upper() in uppers and idx < uppers[ch.upper()]:
                 special += 1
         return special
