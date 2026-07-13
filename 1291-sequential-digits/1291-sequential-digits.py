40ad7a79c1f54bc3a891dab1b4f027b9794781d3
Fasika Gebrehana <fasikagebrehana1@gmail.com>
2026-07-13 16:08:09 +0300

    Time: 0 ms (100%), Space: 19.1 MB (88.24%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,8 +1,18 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        possibilities = [12,23,34,45,56,67,78,89,123,234,345,456,567,678,789,1234,2345,3456,4567,5678,6789,12345,23456,34567,45678,56789,123456,234567,345678,456789,1234567,2345678,3456789,12345678,23456789,123456789]
         ans = []
-        for i in range(len(possibilities)):
-            if possibilities[i] >= low and possibilities[i] <= high:
-                ans.append(possibilities[i])
+        # nums = "123456789"
+        # l, r = 0, 8
+        # while l < r and r < 9:
+        #     if low <= int(nums[l:r+1]) <= high:
+        #         ans.append(int(nums[l:r+1]))
+        #         l += 1
+        #         print("xcfgvhj")
+        #     else:
+        #         r -= 1
+        # print(ans)
+        arr = [12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456, 567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789, 12345678, 23456789, 123456789]
+        for i in range(len(arr)):
+            if low<=arr[i] <= high:
+                ans.append(arr[i])
         return ans
